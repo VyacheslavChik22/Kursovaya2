d9536c994f8f94f8987d14576f42d248961c99d0
Вячеслав <asyofka@mail.ru>
2022-08-10 19:59:48 +0300

    Remove

--- src/main/java/com/example/kursovaya2/exception/QuestionAllReadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.example.kursovaya2.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class QuestionAllReadyExistException extends RuntimeException{
-}
